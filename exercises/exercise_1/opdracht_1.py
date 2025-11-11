3e0e7ee3a3b3d5a189ddbcd4ad64b61520d94c62
maaikeg-1 <maaikeg-1>
2025-11-11 15:24:04 +0000

    Update function to include 3 people like

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -4,6 +4,8 @@ def likes(team: list) -> str:
     elif len(team)==1:
         return f"{team[0]} likes this"
     elif len(team)==2:
-        return f"{team[0]} en {team[1]} like this"
+        return f"{team[0]} and {team[1]} like this"
+    elif len(team)==3:
+        return f"{team[0]}, {team[1]} and {team[2]} like this"
     else: 
         return "too much likes, oopsie"
