53c88144bbc4d94c571c1e9ea5b16873c419649e
maaikeg-1 <maaikeg-1>
2025-11-11 15:08:32 +0000

    like functions 2pers

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,9 @@
 def likes(team: list) -> str:
-    return True
+    if team == []: 
+        return "no one likes this"
+    elif len(team)==1:
+        return f"{team[0]} likes this"
+    elif len(team)==2:
+        return f"{team[0]} en {team[1]} like this"
+    else: 
+        return "too much likes, oopsie"
